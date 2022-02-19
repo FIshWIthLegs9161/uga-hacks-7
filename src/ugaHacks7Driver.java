4a23ab1a69a255ff7a8663c19de73390a94cd430
FishWithLegs9161 <tjn92948@uga.edu>
2022-02-19 18:10:05 -0500

    Still setting up libraries
    Finally figured out project structure, added google library, also added jdk into the meta folder

--- src/ugaHacks7Driver.java
+++ src/ugaHacks7Driver.java
@@ -1,1 +1,12 @@
-//testing
+import javafx.application.Application;
+
+public class ugaHacks7Driver {
+
+    public static void main(String[] args) {
+        try {
+            Application.launch(atm.class, args); //this will start the javafx applicaiton
+        } catch (Exception e) {
+            System.err.println(e.getMessage()); //print any errors without crashing the program for debugging
+        }
+    }
+}
